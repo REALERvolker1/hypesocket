9fc18c31b13fa90e24b81ec684bb4460fc318a72
REALERvolker1 <73304952+REALERvolker1@users.noreply.github.com>
2024-07-17 12:49:08 -0500

    Fix test

--- src/lib.rs
+++ src/lib.rs
@@ -6,27 +6,22 @@ mod abstractions;
 pub mod events;
 pub mod hyprctl;
 
-#[cfg(test)]
+#[cfg(all(test, not(feature = "tokio"), not(feature = "async-lite")))]
 mod tests {
     use crate::hyprctl::{Hyprctl, HyprctlSocket};
     #[test]
     fn exec_kitty() {
-        // crate::abstractions::runtime_test(async move {
-        //     let mut connection = HyprctlSocket::new_from_env()
-        //         .await
-        //         .expect("Failed to connect to hyprland!");
+        let mut connection = HyprctlSocket::new_from_env().expect("Failed to connect to hyprland!");
 
-        //     let my_command = Hyprctl::new(None, &["dispatch", "exec", "kitty"]);
-        //     let command_string = my_command.try_as_str().unwrap();
-        //     assert_eq!(command_string, "/dispatch exec kitty\n");
+        let my_command = Hyprctl::new(None, &["dispatch", "exec", "kitty"]);
+        let command_string = my_command.try_as_str().unwrap();
+        assert_eq!(command_string, "/dispatch exec kitty\n");
 
-        //     let output = connection
-        //         .run_hyprctl(&my_command)
-        //         .await
-        //         .expect("Failed to run command!");
+        let output = connection
+            .run_hyprctl(&my_command)
+            .expect("Failed to run command!");
 
-        //     let output_str = String::from_utf8_lossy(&output);
-        //     assert_eq!(output_str, "ok");
-        // });
+        let output_str = String::from_utf8_lossy(&output);
+        assert_eq!(output_str, "ok");
     }
 }
